e64646ef4be26036dc2ee59f00b8681e9a21f3b8
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2014-03-16 01:51:43 +1100

    added a maven project and solution to the first problem

---
+++ crack-interview-ch01/src/main/java/edu/buet/cse/ch01/App01.java
@@ -0,0 +1,30 @@
+package edu.buet.cse.ch01;
+
+public class App01 {
+  private static final int ASCII_SET_SIZE = 256;
+  
+  public static void main(String[] args) {
+    String input = "This is a test";
+    boolean result = isComposedOfUniqueCharacters(input);
+    System.out.println(result);
+  }
+  
+  // we assume that all characters are from ASCII character set
+  public static boolean isComposedOfUniqueCharacters(String input) {
+    boolean result = true;
+    boolean[] status = new boolean[ASCII_SET_SIZE];
+    
+    for (int i = 0, n = input.length(); i < n; i++) {
+      char c = input.charAt(i);
+      
+      if (status[c]) {
+        result = false;
+        break;
+      }
+      
+      status[c] = true;
+    }
+    
+    return result;
+  }
+}
