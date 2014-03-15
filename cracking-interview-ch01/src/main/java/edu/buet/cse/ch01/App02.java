017886df84993a15b660d93b0144cd0ba2e0ce06
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2014-03-16 02:25:31 +1100

    Added solution to problem 2 of chapter 1

---
+++ cracking-interview-ch01/src/main/java/edu/buet/cse/ch01/App02.java
@@ -0,0 +1,28 @@
+package edu.buet.cse.ch01;
+
+/**
+ *
+ * @author shamim
+ */
+public class App02 {
+  public static void main(String... args) {
+    System.out.println(reverse("this is a test"));
+    System.out.println(reverse("shamima ahmed"));
+  }
+  
+  public static String reverse(String input) {
+    char[] charArray = input.toCharArray();
+    
+    for (int i = 0, j = charArray.length - 1; i < j; i++, j--) {
+      swap(charArray, i, j);
+    }
+    
+    return String.valueOf(charArray);
+  }
+  
+  private static void swap(char[] items, int i, int j) {
+    char c = items[i];
+    items[i] = items[j];
+    items[j] = c;
+  }
+}
