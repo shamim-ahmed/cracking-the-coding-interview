540781252760201ba38a3ba4238b85bef764234d
Shamim Ahmed <shamim.buet.cse.99@gmail.com>
2014-03-22 17:13:15 +1100

    Added a new algorithm to replace space with %20 in a string

---
+++ cracking-interview-ch01/src/main/java/edu/buet/cse/ch01/App05_2.java
@@ -0,0 +1,40 @@
+package edu.buet.cse.ch01;
+
+/**
+ *
+ * @author shamim
+ */
+public class App05_2 {
+  public static void main(String... args) {
+    String input = "This is a test";
+    System.out.println(replaceSpace(input));
+  }
+  
+  private static String replaceSpace(String input) {
+    char[] inputChars = input.toCharArray();
+    int count = 0;
+    
+    for (char c : inputChars) {
+      if (c == ' ') {
+        count++;
+      }
+    }
+    
+    char[] outputChars = new char[inputChars.length + 2 * count];
+    int index = 0;
+    
+    for (int k = 0; k < inputChars.length; k++) {
+      char c = inputChars[k];
+      
+      if (c == ' ') {
+        outputChars[index++] = '%';
+        outputChars[index++] = '2';
+        outputChars[index++] = '0';
+      } else {
+        outputChars[index++] = c;
+      }
+    }
+    
+    return String.valueOf(outputChars);
+  }
+}
